7919de179a5535987b33ad31896d96a2f1b0fb8e
Naiwen Xiao <naiwen.xiao@spc.ox.ac.uk>
2025-08-18 17:18:26 +0800

    Finish the hw06 and lab09

--- HW/hw07/hw07.scm
+++ HW/hw07/hw07.scm
@@ -1,15 +1,30 @@
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp) 
+  (
+    cond
+      ((= exp 1) base)
+      ((even? exp) (pow (square base) (quotient exp 2)))
+      (else (* base (pow (square base) (quotient exp 2)))) 
+  )
+)
 
 (define (repeatedly-cube n x)
   (if (zero? n)
       x
-      (let (_________________)
-        (* y y y))))
+      (let (
+          (y (repeatedly-cube (- n 1) x))
+          )
+          (* y y y)
+      )
+  )
+)
 
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) 
+  (car(cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s)
+  (car (cdr (cdr s)))
+)
--- Lab/lab09/lab09.scm
+++ Lab/lab09/lab09.scm
@@ -1,10 +1,45 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+(
+  if 
+  (< num1 num2) -1
+                (if
+                (= num1 num2) 0 1
+                )
+))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
 
-(define (composed f g) 'YOUR-CODE-HERE)
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+
+
+
+(define (make-adder num)
+        (lambda (inc) (+ num inc))
+)
+
+
+
+
+
+(define (composed f g)
+(
+  lambda (x) (f (g x))
+)
+)
+
+
+
+
+(define (repeat f n) 
+  (
+    define (temp x)
+      (if
+        (= n 1) (f x) (f ((repeat f (- n 1)) x))) 
+  )
+  temp
+)
+
+
+
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +51,9 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b) 
+  (if
+    (>= a b) (if (= 0 (modulo a b)) b (gcd b (modulo a b)))
+    (if (= 0 (modulo b a)) a (gcd a (modulo b a)))
+  )
+)
