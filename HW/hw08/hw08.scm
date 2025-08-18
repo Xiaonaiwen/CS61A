41fc48ae4b220bc55599321e673191459ea78b1d
Naiwen Xiao <naiwen.xiao@spc.ox.ac.uk>
2025-08-18 19:57:26 +0800

    Finish hw08

--- HW/hw08/hw08.scm
+++ HW/hw08/hw08.scm
@@ -1,7 +1,46 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s)
+  (if (null? s)
+      #t
+      (if (null? (cdr s))
+          #t
+          (and (<= (car s) (car (cdr s))) (ascending? (cdr s))))))
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (my-filter pred s)
+    (
+        if (null? s) s
+    (
+        if (pred (car s)) 
+            (cons (car s) (my-filter pred (cdr s)))
+            (my-filter pred (cdr s))
+    )
+    )
+)
+
+
+
+
+
+(define (interleave lst1 lst2)
+    (
+        if (null? lst1) (
+            if(null? lst2) nil lst2
+        )
+        (
+            if(null? lst2) lst1 (cons (car lst1) (interleave lst2 (cdr lst1))) 
+        )
+    )
+)
+
+
+
+
+
+(define (no-repeats s)
+    (
+        if (null? s) s (
+            cons (car s) (no-repeats (filter (lambda (x) (not (= x (car s)))) s))
+        )
+    )
+)
