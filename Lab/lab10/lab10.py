89d0e5137c7c00741271319d98df7bd2fe5c830b
Naiwen Xiao <naiwen.xiao@spc.ox.ac.uk>
2025-08-19 11:42:50 +0800

    Finish lab10

--- Lab/lab10/lab10.py
+++ Lab/lab10/lab10.py
@@ -14,20 +14,22 @@ def calc_eval(exp):
     3
     """
     if isinstance(exp, Pair):
-        operator = ____________ # UPDATE THIS FOR Q2, e.g (+ 1 2), + is the operator
-        operands = ____________ # UPDATE THIS FOR Q2, e.g (+ 1 2), 1 and 2 are operands
+        operator = exp.first # UPDATE THIS FOR Q2, e.g (+ 1 2), + is the operator
+        operands = exp.rest # UPDATE THIS FOR Q2, e.g (+ 1 2), 1 and 2 are operands
         if operator == 'and': # and expressions
             return eval_and(operands)
         elif operator == 'define': # define expressions
             return eval_define(operands)
-        else: # Call expressions
-            return calc_apply(___________, ___________) # UPDATE THIS FOR Q2, what is type(operator)?
+        elif operator in OPERATORS: # Call expressions
+            return calc_apply(OPERATORS[operator], operands.map(calc_eval)) # UPDATE THIS FOR Q2, what is type(operator)?
+        else:
+            return calc_eval(Pair(bindings[operator], operands))
     elif exp in OPERATORS:   # Looking up procedures
         return OPERATORS[exp]
     elif isinstance(exp, int) or isinstance(exp, bool):   # Numbers and booleans
         return exp
-    elif _________________: # CHANGE THIS CONDITION FOR Q4 where are variables stored?
-        return _________________ # UPDATE THIS FOR Q4, how do you access a variable?
+    elif exp in bindings.keys(): # CHANGE THIS CONDITION FOR Q4 where are variables stored?
+        return bindings[exp] # UPDATE THIS FOR Q4, how do you access a variable?
 
 def calc_apply(op, args):
     return op(args)
@@ -52,6 +54,13 @@ def floor_div(args):
     20
     """
     "*** YOUR CODE HERE ***"
+    result = args.first
+    divisor = args.rest
+    while divisor:
+        result = result // divisor.first
+        divisor = divisor.rest
+    return result
+
 
 scheme_t = True   # Scheme's #t
 scheme_f = False  # Scheme's #f
@@ -74,6 +83,16 @@ def eval_and(expressions):
     True
     """
     "*** YOUR CODE HERE ***"
+    if expressions is nil:
+        return scheme_t
+    elif expressions.rest is nil:
+        return expressions.first
+    else:
+        if expressions.first is scheme_f:
+            return expressions.first
+        else:
+            return eval_and(expressions.rest.map(calc_eval))
+
 
 bindings = {}
 
@@ -93,6 +112,14 @@ def eval_define(expressions):
     2
     """
     "*** YOUR CODE HERE ***"
+    symbol = expressions.first
+    temp = expressions.rest.first
+    if temp in OPERATORS:
+        bindings[symbol] = temp
+    else:
+        bindings[symbol] = calc_eval(temp)
+    return symbol
+
 
 OPERATORS = { "//": floor_div, "+": addition, "-": subtraction, "*": multiplication, "/": division }
 
